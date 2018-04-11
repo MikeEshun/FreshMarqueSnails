264d34b960278e7db400f208de35398b089e5f47
Michael Elvis Eshun <dev.mike.eshun@gmail.com>
2018-04-11 09:36:02 +0000

    Initial project setup commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FreshMarqueSnails.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
