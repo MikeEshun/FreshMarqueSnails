926ba62449e909e02b110ccf4f852c58b84b0b91
Michael Elvis Eshun <dev.mike.eshun@gmail.com>
2018-04-11 12:28:21 +0000

    CustomerOrder migration

---
+++ Migrations/20180411101946_OrderCustomer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FreshMarqueSnails.Migrations
+{
+    public partial class OrderCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerOrder",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Comments = table.Column<string>(nullable: true),
+                    Cost = table.Column<decimal>(nullable: false),
+                    DeliveryDate = table.Column<DateTime>(nullable: false),
+                    DeliveryStatus = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    OrderDate = table.Column<DateTime>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerOrder", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerOrder");
+        }
+    }
+}
--- Models/CustomerOrder.cs
+++ Models/CustomerOrder.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FreshMarqueSnails.Models
 {
     public class CustomerOrder
     {
         public int ID { get; set; }
+        
+        [Display(Name = "Order Date")]
+        [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
+        
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
+        
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
         public string Location { get; set; }
+        
+        [Display(Name = "Delivery Date")]
+        [DataType(DataType.Date)]
         public DateTime DeliveryDate { get; set; }
+        
         public int Quantity { get; set; }
+        
+        [Range(1, 10000)]
+        [DataType(DataType.Currency)]
         public decimal Cost { get; set; }
+        
+        [StringLength(10, MinimumLength = 2)]
+        [Required]
         public string DeliveryStatus { get; set; }
+        
+        [StringLength(100, MinimumLength = 0)]
         public string Comments { get; set; }
     }
 }
