cbea3d1a1290c40cb046a4d4520f926c66d480c6
Michael Elvis Eshun <dev.mike.eshun@gmail.com>
2018-04-11 10:22:16 +0000

    CustomerOrder model

---
+++ Models/CustomerOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FreshMarqueSnails.Models
+{
+    public class CustomerOrder
+    {
+        public int ID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int Quantity { get; set; }
+        public decimal Cost { get; set; }
+        public string DeliveryStatus { get; set; }
+        public string Comments { get; set; }
+    }
+}
---
+++ Models/CustomerOrderContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FreshMarqueSnails.Models
+{
+    public class CustomerOrderContext : DbContext
+    {
+        public CustomerOrderContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<CustomerOrder> CustomerOrder { get; set; }
+    }
+}
